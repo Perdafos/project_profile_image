6f5b8a052e795ba2b811efb3072dd2632d7a5226
perdafos <yogatamadafa9@gmail.com>
2025-05-06 12:30:41 +0700

    responsif

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,8 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap');
+
+        .onest {
+            font-family: "Onest", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: <weight>;
+            font-style: normal;
+        }
+    </style>
     @yield('css')
 </head>
 
@@ -18,7 +27,8 @@
     <section class="content">
         @yield('content')
     </section>
-{{-- 
+    {{-- 
     @include('layout.footer') --}}
-@yield('js')
+    @yield('js')
+
 </html>
